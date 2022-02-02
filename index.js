8378d9881c25b40fcd007304a6384afb6b744b5e
Riya Kumari <56725036+Riya-code@users.noreply.github.com>
2022-02-02 21:50:09 +0530

    update-1

--- index.js
+++ index.js
@@ -45,25 +45,3 @@ function get() {
 
 }
 
-function update() {
-  var username = document.getElementById('username').value
-  var email = document.getElementById('email').value
-  var password = document.getElementById('password').value
-
-  var updates = {
-    email : email,
-    password : password
-  }
-
-  database.ref('users/' + username).update(updates)
-
-  alert('updated')
-}
-
-function remove() {
-  var username = document.getElementById('username').value
-
-  database.ref('users/' + username).remove()
-
-  alert('deleted')
-}
